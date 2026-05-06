aabd4fa564ccadfef97c7269ee003583fe1e872a
Guilherme Loureiro <gpmloureiro@gmail.com>
2026-05-06 19:04:26 +0100

    Chore: Express Node.js Setup

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/health', (req, res) => res.json({ status: 'ok' }));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server running on port 3000');
+});
